40e02817ae1b707c2c10e1c5954b13b16cbcdf06
BayuAldiansyah1 <64621381+BayuAldiansyah1@users.noreply.github.com>
2020-05-12 05:55:13 +0800

    Create Jobsheet 1 C.c
    Menampilkan data dari array yang diinput secara statis

---
+++ Jobsheet 1 C.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main (){
+	int myarray [5] = {1,23,17,4,-5};
+	printf("Data ke-1 =%d\n",myarray[0]);
+	printf("Data ke-2 =%d\n",myarray[1]);
+	printf("Data ke-3 =%d\n",myarray[2]);
+	printf("Data ke-4 =%d\n",myarray[3]);
+	printf("Data ke-5 =%d\n",myarray[4]);
+}
