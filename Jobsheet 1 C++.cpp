f25a44515e28327fa47e9177ef3485a8861b08d8
BayuAldiansyah1 <64621381+BayuAldiansyah1@users.noreply.github.com>
2020-05-12 05:56:56 +0800

    Create Jobsheet 1 C++.cpp
    Menampilkan data dari array yang diinput secara statis

---
+++ Jobsheet 1 C++.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main (){
+	int myarray [5] = {1,23,17,4,-5};
+   	cout<<"Nama=Bayu Aldiansyah\nNIM=5\nKelompok=8";
+	cout<<"\nData ke-1 ="<<myarray[0]<<endl;
+	cout<<"Data ke-2 ="<<myarray[1]<<endl;
+	cout<<"Data ke-3 ="<<myarray[2]<<endl;
+	cout<<"Data ke-4 ="<<myarray[3]<<endl;
+	cout<<"Data ke-5 ="<<myarray[4]<<endl;
+}
