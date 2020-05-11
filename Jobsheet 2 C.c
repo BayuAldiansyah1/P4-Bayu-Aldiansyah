fd2b8ee490e7e6fc2be621b949243591479b9b63
BayuAldiansyah1 <64621381+BayuAldiansyah1@users.noreply.github.com>
2020-05-12 05:58:46 +0800

    Create Jobsheet 2 C.c
    Menampilkan data dari array dua dimensi

---
+++ Jobsheet 2 C.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main (){
+	int bilangan [6][5] = {{1,2,3,4,5},{6,7,8,9,10},{11,12,13,14,15},{16,17,18,19,20},{21,22,23,24,25},{26,27,28,29,30}};
+   	printf("Nama=Bayu\nNIM=5\n");
+   	printf("Nama=Heru\nNIM=6\n");
+   	for (int i=0;i<6;i++){
+   		for(int j=0;j<5;j++){
+   			printf("isi array ke [%i][%i] = %i\n",i,j,bilangan[i][j]);
+ 	   }
+	   }
+	}
